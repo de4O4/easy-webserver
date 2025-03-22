d395772a316ae85dfa910676ce237946f8cdd0d6
de4O4 <1195525350@qq.com>
2025-03-22 16:41:25 +0800

    日志实现

--- log/log.cpp
+++ log/log.cpp
@@ -40,5 +40,82 @@ bool Log::init(const char *file_name , int close_log , int log_buf_size , int sp
     if(p == NULL){      //当日志文件不存在 则创建一个日志文件
         snprintf(log_full_name , 255 , "%s%d_%02d_%02d_%s" , dir_name , my_ym.tm_year + 1900 , my_ym.tm_mon + 1 , my_ym.tm_mday , file_name);       //格式化log_full_name
     }
+    else{
+        strcpy(log_full_name , p+1);        //复制文件名部分（去掉路径部分）到 log_name 中。
+        strncpy(dir_name , file_name , p - file_name + 1);      //将路径部分复制到 dir_name 中
+        snprintf(log_full_name , 255 , "%s%d_%02d_%02d_%s" , dir_name , my_ym.tm_year + 1900 , my_ym.tm_mon + 1 , my_ym.tm_mday , file_name); 
+    }
+    m_today = my_ym.tm_mday;
+    m_fp = fopen(log_full_name , "a");
+    if(m_fp == NULL){
+        return false;
+    }
+    return true;
+}
 
+void Log::write_log(int level , const char *format , ...){
+    struct timeval now = {0 , 0};
+    gettimeofday(&now , NULL);      //获取了系统的秒级和微秒级时间
+    time_t t = now.tv_sec;          //time_t 通常用来存储 Unix 时间戳 自 Unix 纪元（1970 年 1 月 1 日）以来的秒数。
+    struct tm *sys_tm = localtime(&t);      //localtime 函数将秒级时间转化为可读的本地时间格式
+    struct tm my_tm = *sys_tm;
+    char s[16] = {0};
+    switch (level)
+    {
+    case 0:
+        strcpy(s , "[debug]:");
+        break;
+    case 1:
+        strcpy(s , "[info]:");
+        break;
+    case 2:
+        strcpy(s , "[warn]:");
+        break;
+    case 3:
+        strcpy(s , "[erro]:");
+        break;
+    default:
+        strcpy(s , "[info]:");
+        break;
+    }
+    m_mutex.lock();
+    m_cout++;
+    if(m_today != my_tm.tm_mday || m_cout % m_split_lines == 0){
+        char new_log[256] = {0};        //用来存储新的日志文件路径
+        fflush(m_fp);
+        fclose(m_fp);
+        char tail[16] = {0};        //用来存储日志文件名的日期部分
+        snprintf(tail , 16 , "%d_%02d_%02d_" , my_tm.tm_year + 1900 , my_tm.tm_mon + 1 , my_tm.tm_mday);
+        if(m_today != my_tm.tm_mday){
+            snprintf(new_log , 255 , "%s%s%s" , dir_name , tail , log_name);
+            m_today = my_tm.tm_mday;
+            m_cout = 0;
+        }
+        else{
+            snprintf(new_log , 255 , "%s%s%s.%lld" , dir_name , tail , log_name , m_cout / m_split_lines);
+        }
+        m_fp = fopen(new_log , "a");
+    }
+    m_mutex.unlock();
+    va_list valst;
+    va_start(valst , format);       //初始化 valst 变量，使其指向函数中第一个可变参数
+
+    string log_str;
+    m_mutex.lock();
+
+    int n = snprintf(m_buf , 48 , "%d-%02d-%02d %02d:%02d:%02d.%06ld %s " , my_tm.tm_yday + 1900 , my_tm.tm_mon + 1 , my_tm.tm_mday , my_tm.tm_hour , my_tm.tm_min , my_tm.tm_sec , now.tv_usec , s);       //写入具体的时间格式
+    int m = vsnprintf(m_buf + n , m_log_buf_size - n -1 , format , valst);
+    m_buf[n + m] = '\n';        //将换行符添加到缓冲区，表示一行日志的结束
+    m_buf[n + m + 1] = '\0';        //追加字符串结束符，确保缓冲区内容是一个有效的 C 字符串
+    log_str = m_buf;
+    m_mutex.unlock();
+
+    if(m_is_async && !m_log_queue->full()){     //如果异步日志则加入阻塞队列
+        m_log_queue->push(log_str);
+    }
+    else{           //同步日志写入操作
+        m_mutex.lock();
+        fputs(log_str.c_str() , m_fp);
+        m_mutex.unlock();
+    }
 }
--- log/log.h
+++ log/log.h
@@ -46,7 +46,7 @@ class Log{                 //log类
             Log::get_instance()->async_write_log();
         }
         bool init(const char *file_name , int close_log , int log_buf_size = 8192 , int split_lines = 5000000 , int max_queue_szie = 0);
-        void write_log(int level , const char &format , ...);
+        void write_log(int level , const char *format , ...);
         void flush(void);
 };
 
